15ea1578ac22db170518205540dd50464bb8e3dd
stesen <stesenchina@gmail.com>
2012-05-15 22:54:24 +0800

    fix volume.lisp bug

--- small-scripts/volume/volume.lisp
+++ small-scripts/volume/volume.lisp
@@ -1,7 +1,8 @@
 #!/usr/bin/clisp
 
-(read-line
- (ext:shell
-  "aumix -n get Master 2>/dev/null \ egrep -o \"[0-9]{1,3}%\" | tail -n 1"
-  :wait nil
-  :output :stream))
+(format t "~a~%"
+	(read-line
+	 (ext:run-shell-command
+	  "amixer -n get Master 2>/dev/null | egrep -o \"[0-9]{1,3}%\" | tail -n 1"
+	  :wait nil
+	  :output :stream)))
